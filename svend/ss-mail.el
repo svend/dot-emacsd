cc4e4e05eb2fb096b476cf9b95dbe1a50fcd4a12
Svend Sorensen <svend@ciffer.net>
2012-03-23 11:24:25 -0700

    Update

---
+++ svend/ss-mail.el
@@ -0,0 +1,30 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Mail/message-mode settings
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Close mail buffer after sending
+;; http://notmuchmail.org/emacstips/#index3h2
+(setq message-kill-buffer-on-exit t)
+
+;; Use MSMTP with auto-smtp selection
+;; http://www.emacswiki.org/emacs/GnusMSMTP#toc3
+;;
+;; 'message-sendmail-f-is-evil nil' is the default on Debian squeeze
+;; (emacs >= 23.2+1-5)
+(setq sendmail-program "/usr/bin/msmtp"
+      mail-specify-envelope-from t
+      mail-envelope-from 'header
+      message-sendmail-envelope-from 'header)
+
+;; Enable flyspell for composing mail
+;; http://www.gnus.org/manual/big-message.html#SEC32
+(add-hook 'message-setup-hook (lambda() (flyspell-mode t)))
+
+;; Gnome terminal seems to translate M-RET to C-M-j
+(add-hook 'message-setup-hook
+	  (lambda ()
+	    (if window-system
+		nil
+	      (progn
+		(define-key message-mode-map "\C-\M-j" 'message-newline-and-reformat)))))
+
