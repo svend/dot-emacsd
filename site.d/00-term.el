bbd722a62ea8783ec7e8681c98450cfe9cc872b8
Svend Sorensen <svend@ciffer.net>
2012-03-27 10:04:51 -0700

    Tweak term mode

--- site.d/00-term.el
+++ site.d/00-term.el
@@ -2,6 +2,7 @@
 	  '(lambda ()
 	     ;; Make term mode more term-like
 	     ;;
-	     ;; Send <ESC> and C-c directly to the terminal.
-	     (define-key term-raw-map "\e" 'term-send-raw)
+	     ;; Send ESC and C-c directly to the terminal.
+	     ;; TODO: Sending ESC raw causes M-. to be processed by emacs
+	     ;;(define-key term-raw-map "\e" 'term-send-raw)
 	     (define-key term-raw-map "\C-c" 'term-send-raw)))
