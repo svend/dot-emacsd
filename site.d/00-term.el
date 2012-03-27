09e3e76a705e1c10652d3b465609f5bbf70d9268
Svend Sorensen <svend@ciffer.net>
2012-03-27 13:40:49 -0700

    More term tweaks

--- site.d/00-term.el
+++ site.d/00-term.el
@@ -1,8 +1,17 @@
 (add-hook 'term-mode-hook
 	  '(lambda ()
-	     ;; Make term mode more term-like
+	     ;; Disable transient mark mode
 	     ;;
-	     ;; Send ESC and C-c directly to the terminal.
+	     ;; This is advised in the term.el source. See
+	     ;; http://bzr.savannah.gnu.org/lh/emacs/emacs-23/annotate/head:/lisp/term.el
+	     (make-local-variable 'transient-mark-mode)
+	     (setq transient-mark-mode nil)
+
+	     ;; Make term mode more term-like
+
+	     ;; Send ESC to the terminal
 	     ;; TODO: Sending ESC raw causes M-. to be processed by emacs
-	     ;;(define-key term-raw-map "\e" 'term-send-raw)
+	     ;; (define-key term-raw-map "\e" 'term-send-raw)
+
+	     ;; Send C-c to the terminal
 	     (define-key term-raw-map "\C-c" 'term-send-raw)))
