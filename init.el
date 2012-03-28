d189c30b0861806518223f98f874c243a86858de
Svend Sorensen <svend@ciffer.net>
2012-03-27 21:07:38 -0700

    Rename config dir

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defvar emacs-conf-path
-  "~/.emacs.d/site.d"
+  "~/.emacs.d/init.d"
   "The full path to the emacs config directory")
 
 (dolist (file (directory-files emacs-conf-path t "^[0-9]+.*\\.el$"))
