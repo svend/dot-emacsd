60f5a6361ac06562f14b522f20b2088b89122e30
Svend Sorensen <svend@ciffer.net>
2012-04-02 10:23:16 -0700

    Move keybindings to their own file

---
+++ init.d/00-keybindings.el
@@ -0,0 +1,10 @@
+;; Add some convenient window mangement bindings
+;; Make C-tab act like C-x o
+(global-set-key (kbd "C-<tab>") 'other-window)
+;; Make C-n act like C-x n
+(global-set-key (kbd "C-1") 'delete-other-windows)
+(global-set-key (kbd "C-2") 'split-window-vertically)
+(global-set-key (kbd "C-3") 'split-window-horizontally)
+(global-set-key (kbd "C-4") 'ctl-x-4-prefix)
+(global-set-key (kbd "C-5") 'ctl-x-5-prefix)
+(global-set-key (kbd "C-0") 'delete-window)
--- init.d/00-misc.el
+++ init.d/00-misc.el
@@ -19,17 +19,6 @@
 ;; Enable disabled commands
 (put 'dired-find-alternate-file 'disabled nil)
 
-;; Add some convenient window mangement bindings
-;; Make C-tab act like C-x o
-(global-set-key (kbd "C-<tab>") 'other-window)
-;; Make C-n act like C-x n
-(global-set-key (kbd "C-1") 'delete-other-windows)
-(global-set-key (kbd "C-2") 'split-window-vertically)
-(global-set-key (kbd "C-3") 'split-window-horizontally)
-(global-set-key (kbd "C-4") 'ctl-x-4-prefix)
-(global-set-key (kbd "C-5") 'ctl-x-5-prefix)
-(global-set-key (kbd "C-0") 'delete-window)
-
 ;; Use GUI web browser if it is available
 (if (executable-find "x-www-browser")
     (progn
