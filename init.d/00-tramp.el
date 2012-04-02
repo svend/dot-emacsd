6467fffb4e11b69e80e667f678c6a968b3ef6b3f
Svend Sorensen <svend@ciffer.net>
2012-04-02 10:20:20 -0700

    Update

--- init.d/00-tramp.el
+++ init.d/00-tramp.el
@@ -1,11 +1,13 @@
 (eval-after-load "tramp"
   '(progn
-
      ;; Edit remote files via sudo
+     ;;
+     ;; Enable editing of remote files as root using sudo using the
+     ;; address /sudo:host.example.org:.
+     ;;
      ;; http://www.gnu.org/software/tramp/#Multi_002dhops
-    (add-to-list 'tramp-default-proxies-alist
-		  '((nil "\\`root\\'" "/ssh:%h:")))
-    (add-to-list 'tramp-default-proxies-alist
-		  '((regexp-quote (system-name)) nil nil))
-
+     (add-to-list 'tramp-default-proxies-alist
+                  '(nil "\\`root\\'" "/ssh:%h:"))
+     (add-to-list 'tramp-default-proxies-alist
+                  '((regexp-quote (system-name)) nil nil))
      ))
