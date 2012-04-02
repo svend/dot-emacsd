75777f93ae81cf4e90c3a3c6f5094a4e6400b3c2
Svend Sorensen <svend@ciffer.net>
2012-04-02 16:08:23 -0700

    eval-after-load

--- init.d/00-recentf.el
+++ init.d/00-recentf.el
@@ -2,25 +2,29 @@
 ;;
 ;; http://www.masteringemacs.org/articles/2011/01/27/find-files-faster-recent-files-package/
 
-;; Put recentf save file under ~/.cache
-(setq recentf-save-file "~/.cache/emacs")
-
-;; Ignore git commit messages
-(add-to-list 'recentf-exclude "^COMMIT_EDITMSG$")
-
-;; get rid of `find-file-read-only' and replace it with something
-;; more useful.
-(global-set-key (kbd "C-x C-r") 'ido-recentf-open)
-
 ;; enable recent files mode.
 (recentf-mode t)
 
-;; 50 files ought to be enough (default is 20)
-(setq recentf-max-saved-items 50)
+(eval-after-load "recentf"
+  '(progn
+     ;; Put recentf save file under ~/.cache and reload
+     (setq recentf-save-file "~/.cache/emacs/.recentf")
+     (recentf-load-list)
 
-(defun ido-recentf-open ()
-  "Use `ido-completing-read' to \\[find-file] a recent file"
-  (interactive)
-  (if (find-file (ido-completing-read "Find recent file: " recentf-list))
-      (message "Opening file...")
-    (message "Aborting")))
+     ;; Ignore ephemeral git commit message files
+     (add-to-list 'recentf-exclude "^COMMIT_EDITMSG$")
+
+     ;; get rid of `find-file-read-only' and replace it with something
+     ;; more useful.
+     (global-set-key (kbd "C-x C-r") 'ido-recentf-open)
+
+     ;; 50 files ought to be enough (default is 20)
+     (setq recentf-max-saved-items 50)
+
+     (defun ido-recentf-open ()
+       "Use `ido-completing-read' to \\[find-file] a recent file"
+       (interactive)
+       (if (find-file (ido-completing-read "Find recent file: " recentf-list))
+	   (message "Opening file...")
+	 (message "Aborting")))
+     ))
