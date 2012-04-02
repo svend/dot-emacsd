69403988261579a5199494bfd4a946ab04d36340
Svend Sorensen <svend@ciffer.net>
2012-04-02 12:33:01 -0700

    Enable recentf mode with ido support

---
+++ init.d/00-recentf.el
@@ -0,0 +1,20 @@
+;; Enable recentf mode with ido-mode support
+;;
+;; http://www.masteringemacs.org/articles/2011/01/27/find-files-faster-recent-files-package/
+
+;; get rid of `find-file-read-only' and replace it with something
+;; more useful.
+(global-set-key (kbd "C-x C-r") 'ido-recentf-open)
+
+;; enable recent files mode.
+(recentf-mode t)
+
+;; 50 files ought to be enough (default is 20)
+;; (setq recentf-max-saved-items 50)
+
+(defun ido-recentf-open ()
+  "Use `ido-completing-read' to \\[find-file] a recent file"
+  (interactive)
+  (if (find-file (ido-completing-read "Find recent file: " recentf-list))
+      (message "Opening file...")
+    (message "Aborting")))
