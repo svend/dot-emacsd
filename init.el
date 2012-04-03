1fcd6775dd0fa059916ff4610e386304d2f61c12
Svend Sorensen <svend@ciffer.net>
2012-04-03 15:10:02 -0700

    Rename var

--- init.el
+++ init.el
@@ -1,9 +1,9 @@
 ;; Emacs settings
 
-(defvar emacs-user-init-path
+(defvar user-emacs-init-path
   "~/.emacs.d/init.d"
   "The full path to user init directory")
 
 ;; Load all n*.el files under user init directory
-(dolist (file (directory-files emacs-user-init-path t "^[0-9]+.*\\.el$"))
+(dolist (file (directory-files user-emacs-init-path t "^[0-9]+.*\\.el$"))
   (load file))
