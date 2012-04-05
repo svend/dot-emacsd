d669252bd293caabd3966f26aba2620f46b09775
Svend Sorensen <svend@ciffer.net>
2012-04-05 14:50:14 -0700

    Enable solarized
    I'll just reload it when I run into the color problem with
    emacsclient.
    diff --gitdiff --git a/init.d/60-color-theme.el b/init.d/60-color-theme.el
    index 3c0632e..f4beaf3 100644
    --- a/init.d/60-color-theme.el
    +++ b/init.d/60-color-theme.el
    @@ -11,7 +11,7 @@
     ;; TODO: Getting neon colors when running emacsclient. This only
     ;; happens with the solarized theme. Loading the theme again fixes the
     ;; problem.
    -;; (color-theme-solarized-light)
    +(color-theme-solarized-light)
    
     ;; This works:
     ;; (require 'color-theme)
     a/init.d/60-color-theme.el b/init.d/60-color-theme.el
    index 3c0632e..f4beaf3 100644
    --- a/init.d/60-color-theme.el
    +++ b/init.d/60-color-theme.el
    @@ -11,7 +11,7 @@
     ;; TODO: Getting neon colors when running emacsclient. This only
     ;; happens with the solarized theme. Loading the theme again fixes the
     ;; problem.
    -;; (color-theme-solarized-light)
    +(color-theme-solarized-light)
    
     ;; This works:
     ;; (require 'color-theme)

--- init.d/60-color-theme.el
+++ init.d/60-color-theme.el
@@ -11,7 +11,7 @@
 ;; TODO: Getting neon colors when running emacsclient. This only
 ;; happens with the solarized theme. Loading the theme again fixes the
 ;; problem.
-;; (color-theme-solarized-light)
+(color-theme-solarized-light)
 
 ;; This works:
 ;; (require 'color-theme)
