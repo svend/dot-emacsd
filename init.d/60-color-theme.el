7bffdb6f7a280abaaf54c7bd2c081388ebfe2728
Svend Sorensen <svend@ciffer.net>
2012-04-05 13:15:20 -0700

    Init color-theme after it loads

--- init.d/60-color-theme.el
+++ init.d/60-color-theme.el
@@ -1,4 +1,10 @@
 ;; This file must come after the color-theme autoloads
 
+;; Initialize color theme after it loads
+(eval-after-load 'color-theme
+  '(progn
+     (color-theme-initialize)
+     ))
+
 ;; Trying the solarized (light) color theme
 (color-theme-solarized-light)
