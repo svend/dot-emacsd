d46e22a398019489b15e79f442394fee868c751c
Svend Sorensen <svend@ciffer.net>
2012-04-05 13:12:48 -0700

    Term tweaks

--- init.d/00-term.el
+++ init.d/00-term.el
@@ -7,21 +7,33 @@
 	     (make-local-variable 'transient-mark-mode)
 	     (setq transient-mark-mode nil)
 
+	     ;; Disable hl-line-mode in term buffers
+	     (make-local-variable 'global-hl-line-mode)
+	     (setq global-hl-line-mode nil)
+
+	     ;; Disable line wrapping
+	     (setq truncate-lines t)
+
 	     ;; Make term mode more term-like
 
 	     ;; Send ESC to the terminal
+	     ;;
 	     ;; TODO: Sending ESC raw causes M-. to be processed by emacs
 	     ;; (define-key term-raw-map "\e" 'term-send-raw)
 
 	     ;; Send C-c to the terminal
-	     (define-key term-raw-map "\C-c" 'term-send-raw)
+	     (define-key term-raw-map (kbd "C-c") 'term-send-raw)
 
-	     ;; Make the terminal forground and background colors
-	     ;; match the other windows
+	     ;; Allow pasting in term mode
 	     ;;
-	     ;; This is needed when loading a color-theme.
-	     (setq term-default-bg-color (frame-parameter nil 'background-color))
-	     (setq term-default-fg-color (frame-parameter nil 'foreground-color))
+	     ;; TODO: Disabled. This conflicts with readline C-y,
+	     ;; which is useful.
+	     ;; (define-key term-raw-map (kbd "C-y") 'term-paste)
 
-	     ;; Disable hl-line-mode in term buffers
-	     (set (make-local-variable 'global-hl-line-mode) nil)))
+	     ;; Toogle between line and char mode in term-mode
+	     (define-key term-raw-map  (kbd "C-'") 'term-line-mode)
+	     (define-key term-mode-map (kbd "C-'") 'term-char-mode)
+
+	     ;; Use configured color theme
+	     (setq term-default-bg-color (frame-parameter nil 'background-color))
+	     (setq term-default-fg-color (frame-parameter nil 'foreground-color))))
