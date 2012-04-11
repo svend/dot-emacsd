abb17a8a67035503d3d27bfab1f1cb4f41cbf58d
Svend Sorensen <svend@ciffer.net>
2012-04-11 16:13:09 -0700

    Add x-identity for gnus-identities

--- gnus.el
+++ gnus.el
@@ -18,7 +18,13 @@
 		(nnimap-expunge-on-close 'never)
 		(nnimap-list-pattern ("INBOX" "mail/*")))))
 
+;; x-identity is for gnus-identities
+;; https://github.com/renard/gnus-identities
 (setq gnus-posting-styles
-      '(("uw"
+      '((".*"
+	 (x-identity "default")
+	 (address user-mail-address))
+	("uw"
+	 (x-identity "uw")
          (address "svends@uw.edu")
          ("Bcc" "svends@uw.edu"))))
