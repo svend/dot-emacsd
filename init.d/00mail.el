2d50095e8af59ff8ad017451c3fa0265edff459f
Svend Sorensen <svend@ciffer.net>
2012-04-12 10:49:52 -0700

    Wrap all message-mode settings in eval-after-load

--- init.d/00mail.el
+++ init.d/00mail.el
@@ -1,39 +1,43 @@
 ;; Mail/message-mode settings
 
-;; Close mail buffer after sending
-(setq message-kill-buffer-on-exit t)
+(eval-after-load 'message
+  '(progn
+     (define-key message-mode-map "\C-c\C-p" 'gnus-identities-change)
 
-;; Use MSMTP with auto-smtp selection
-;; http://www.emacswiki.org/emacs/GnusMSMTP#toc3
-;;
-;; 'message-sendmail-f-is-evil nil' is the default on Debian squeeze
-;; (emacs >= 23.2+1-5)
-(setq sendmail-program "/usr/bin/msmtp"
-      mail-specify-envelope-from t
-      mail-envelope-from 'header
-      message-sendmail-envelope-from 'header)
+     ;; Close mail buffer after sending
+     (setq message-kill-buffer-on-exit t)
 
-;; Enable flyspell for composing mail
-;; http://www.gnus.org/manual/big-message.html#SEC32
-(add-hook 'message-setup-hook (lambda() (flyspell-mode t)))
+     ;; Use MSMTP with auto-smtp selection
+     ;; http://www.emacswiki.org/emacs/GnusMSMTP#toc3
+     ;;
+     ;; 'message-sendmail-f-is-evil nil' is the default on Debian squeeze
+     ;; (emacs >= 23.2+1-5)
+     (setq sendmail-program "/usr/bin/msmtp"
+	   mail-specify-envelope-from t
+	   mail-envelope-from 'header
+	   message-sendmail-envelope-from 'header)
 
-(defun my-message-header-setup-hook ()
-  ;; Canonicalize my UW email address
-  (when (string-match "svends@\\(uw.edu\\|u\\.washington.edu\\|washington\\.edu\\)"
-		      (message-fetch-field "From"))
-    (gnus-identities-change "uw")))
+     ;; Enable flyspell for composing mail
+     ;; http://www.gnus.org/manual/big-message.html#SEC32
+     (add-hook 'message-setup-hook (lambda() (flyspell-mode t)))
 
-(add-hook 'message-header-setup-hook
-	  'my-message-header-setup-hook)
+     (defun my-message-header-setup-hook ()
+       ;; Canonicalize my UW email address
+       (when (string-match "svends@\\(uw.edu\\|u\\.washington.edu\\|washington\\.edu\\)"
+			   (message-fetch-field "From"))
+	 (gnus-identities-change "uw")))
 
-;; Bind C-M-j to message-newline-and-reformat
-;;
-;; Gnome terminal seems to translate M-RET to C-M-j. The GUI shouldn't
-;; have this problem.
-(add-hook 'message-setup-hook
-	  (lambda ()
-	    (if window-system
-		nil
-	      (progn
-		(define-key message-mode-map "\C-\M-j" 'message-newline-and-reformat)))))
+     (add-hook 'message-header-setup-hook
+	       'my-message-header-setup-hook)
+
+     ;; Bind C-M-j to message-newline-and-reformat
+     ;;
+     ;; Gnome terminal seems to translate M-RET to C-M-j. The GUI shouldn't
+     ;; have this problem.
+     (add-hook 'message-setup-hook
+	       (lambda ()
+		 (if window-system
+		     nil
+		   (progn
+		     (define-key message-mode-map "\C-\M-j" 'message-newline-and-reformat)))))))
 
