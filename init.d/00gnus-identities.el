5e3a1baf00dcaedd419f4c331da82a6c32ea98d7
Svend Sorensen <svend@ciffer.net>
2012-04-12 10:36:01 -0700

    Typo

--- init.d/00gnus-identities.el
+++ init.d/00gnus-identities.el
@@ -1,6 +1,6 @@
 (eval-after-load 'gnus-identities
   '(progn
-     ;; gnus-msg must be loaded for gnus-identites
+     ;; gnus-msg must be loaded for gnus-identities
      (require 'gnus-msg)
 
      ;; Read gnus config file; gnus may not have loaded
