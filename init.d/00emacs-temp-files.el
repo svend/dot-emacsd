9ef3e16c52fa681f36583f3bcedac8f84f410953
Svend Sorensen <svend@ciffer.net>
2012-04-13 21:57:09 -0700

    Was wrong about autosave files

--- init.d/00emacs-temp-files.el
+++ init.d/00emacs-temp-files.el
@@ -3,10 +3,14 @@
 ;; Put all backups and auto-saves under ~/.cache
 (setq backup-directory-alist
       `((".*" . ,"~/.cache/emacs/backup/")))
-;; TODO: I'm still getting an occasional auto-save file in the working
-;; directory.
 (setq auto-save-file-name-transforms
       `((".*" ,"~/.cache/emacs/backup/" t)))
 
 ;; Put auto-save list file under ~/.cache
 (setq auto-save-list-file-prefix "~/.cache/emacs/auto-save-list/.saves-")
+
+;; TODO: Lock files, which can be confused for auto-save files are
+;; created in the same directory as the file being edited. I don't
+;; know of any way to disable this feature.
+;;
+;; http://lists.gnu.org/archive/html/emacs-devel/2011-07/msg01020.html
