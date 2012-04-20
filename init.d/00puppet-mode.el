d8e0dea3452fac51fbbc4969585cb874c2eca74e
Svend Sorensen <svend@ciffer.net>
2012-04-19 17:07:05 -0700

    Use eval-after-load instead of require

--- init.d/00puppet-mode.el
+++ init.d/00puppet-mode.el
@@ -1,10 +1,11 @@
 (eval-after-load 'puppet-mode
   '(progn
      (add-hook 'puppet-mode-hook 'flymake-puppet-load)
-     ;; TODO: Get rid of "require"
-     (require 'align)
-     (add-to-list 'align-rules-list
-		  '(puppet-arrow-assignment
-		    (regexp . "\\(\\s-*\\)=>\\(\\s-*\\)")
-		    (group . (1 2))
-		    (modes . '(puppet-mode))))))
+
+     (eval-after-load 'align
+       '(progn
+	  (add-to-list 'align-rules-list
+		       '(puppet-arrow-assignment
+			 (regexp . "\\(\\s-*\\)=>\\(\\s-*\\)")
+			 (group . (1 2))
+			 (modes . '(puppet-mode))))))))
