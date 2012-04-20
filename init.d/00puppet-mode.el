c043e03d7a574973255f75db759c790cef0c1482
Svend Sorensen <svend@ciffer.net>
2012-04-19 17:02:01 -0700

    Add puppet "=>" alignment rules

--- init.d/00puppet-mode.el
+++ init.d/00puppet-mode.el
@@ -1,3 +1,10 @@
 (eval-after-load 'puppet-mode
   '(progn
-     (add-hook 'puppet-mode-hook 'flymake-puppet-load)))
+     (add-hook 'puppet-mode-hook 'flymake-puppet-load)
+     ;; TODO: Get rid of "require"
+     (require 'align)
+     (add-to-list 'align-rules-list
+		  '(puppet-arrow-assignment
+		    (regexp . "\\(\\s-*\\)=>\\(\\s-*\\)")
+		    (group . (1 2))
+		    (modes . '(puppet-mode))))))
