3ba5ea3b1fe8e81a9e82a28b502aedb73858e4b5
Svend Sorensen <svend@ciffer.net>
2012-04-28 23:14:46 -0700

    Add my preferences for sh indentation

---
+++ init.d/00sh.el
@@ -0,0 +1,8 @@
+(defun my-setup-sh-mode ()
+  "My preferences for sh-mode"
+  (interactive)
+  (setq sh-basic-offset 8)
+  (setq sh-indent-for-case-alt '+)
+  (setq sh-indent-for-case-label 0))
+
+(add-hook 'sh-mode-hook 'my-setup-sh-mode)
