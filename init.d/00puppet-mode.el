d812c985ba8b61893e057bd687d4171c37da6a3a
Svend Sorensen <svend@ciffer.net>
2012-05-04 10:30:15 -0700

    Use named function instead of lambda

--- init.d/00puppet-mode.el
+++ init.d/00puppet-mode.el
@@ -2,10 +2,14 @@
   '(progn
      (add-hook 'puppet-mode-hook 'flymake-puppet-load)
 
-     (setq my-puppet-align-rules-list
-	   '((puppet-arrow-assignment
-	      (regexp . "\\(\\s-*\\)=>\\(\\s-*\\)")
-	      (group . (1 2)))))
-     (add-hook 'puppet-mode-hook
-	       (lambda ()
-		 (setq align-mode-rules-list my-puppet-align-rules-list)))))
+     (defun my-setup-puppet-mode ()
+       "My preferences for puppet mode"
+       (interactive)
+       (setq my-puppet-align-rules-list
+	     '((puppet-arrow-assignment
+		(regexp . "\\(\\s-*\\)=>\\(\\s-*\\)")
+		(group . (1 2)))))
+
+       (setq align-mode-rules-list my-puppet-align-rules-list))
+
+     (add-hook 'puppet-mode-hook 'my-setup-puppet-mode)))
