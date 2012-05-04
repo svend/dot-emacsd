c5a55d9b2b5284d3507a240c884b907b3b930570
Svend Sorensen <svend@ciffer.net>
2012-05-04 10:15:53 -0700

    Use named function instead of lamda

--- init.d/00term.el
+++ init.d/00term.el
@@ -1,48 +1,52 @@
 (eval-after-load 'term
   '(progn
-     (add-hook 'term-mode-hook
-	       '(lambda ()
-		  ;; Disable transient mark mode
-		  ;;
-		  ;; This is advised in the term.el source. See
-		  ;; http://git.savannah.gnu.org/cgit/emacs.git/tree/lisp/term.el?id=c720ef1329232c76d14a0c39daa00e37279aa818#n179
-		  (make-local-variable 'transient-mark-mode)
-		  (setq transient-mark-mode nil)
+     (defun my-setup-term-mode ()
+       "My preferences for term mode"
+       (interactive)
 
-		  ;; Disable hl-line-mode in term buffers
-		  (make-local-variable 'global-hl-line-mode)
-		  (setq global-hl-line-mode nil)
+       ;; Disable transient mark mode
+       ;;
+       ;; This is advised in the term.el source. See
+       ;; http://git.savannah.gnu.org/cgit/emacs.git/tree/lisp/term.el?id=c720ef1329232c76d14a0c39daa00e37279aa818#n179
+       (make-local-variable 'transient-mark-mode)
+       (setq transient-mark-mode nil)
 
-		  ;; Disable line wrapping
-		  (setq truncate-lines t)
+       ;; Disable hl-line-mode in term buffers
+       (make-local-variable 'global-hl-line-mode)
+       (setq global-hl-line-mode nil)
 
-		  ;; Make term mode more term-like
+       ;; Disable line wrapping
+       (setq truncate-lines t)
 
-		  ;; Set term-escape to C-q (default is C-c)
-		  ;;
-		  ;; Use this escape to send the next keypress directly to
-		  ;; the terminal.
-		  ;;
-		  ;; TODO: This is supposed to set the existing
-		  ;; term-escape-char to term-send-raw, but from the
-		  ;; term.el code, it doesn't look like term-escape-char is
-		  ;; ever set.
-		  ;;
-		  ;; http://git.savannah.gnu.org/cgit/emacs.git/tree/lisp/term.el?id=c720ef1329232c76d14a0c39daa00e37279aa818#n879
-		  (term-set-escape-char ?\C-q)
+       ;; Make term mode more term-like
 
-		  ;; Send C-c to the terminal
-		  (define-key term-raw-map (kbd "C-c") 'term-send-raw)
+       ;; Set term-escape to C-q (default is C-c)
+       ;;
+       ;; Use this escape to send the next keypress directly to
+       ;; the terminal.
+       ;;
+       ;; TODO: This is supposed to set the existing
+       ;; term-escape-char to term-send-raw, but from the
+       ;; term.el code, it doesn't look like term-escape-char is
+       ;; ever set.
+       ;;
+       ;; http://git.savannah.gnu.org/cgit/emacs.git/tree/lisp/term.el?id=c720ef1329232c76d14a0c39daa00e37279aa818#n879
+       (term-set-escape-char ?\C-q)
 
-		  ;; Send ESC to the terminal
-		  ;;
-		  ;; TODO: Sending ESC raw causes M-. to be processed by emacs
-		  ;; (define-key term-raw-map "\e" 'term-send-raw)
+       ;; Send C-c to the terminal
+       (define-key term-raw-map (kbd "C-c") 'term-send-raw)
 
-		  ;; Toogle between line and char mode in term-mode
-		  (define-key term-raw-map (kbd "C-'") 'term-line-mode)
-		  (define-key term-mode-map (kbd "C-'") 'term-char-mode)
+       ;; Send ESC to the terminal
+       ;;
+       ;; TODO: Sending ESC raw causes M-. to be processed by emacs
+       ;; (define-key term-raw-map "\e" 'term-send-raw)
 
-		  ;; Use configured color theme
-		  (setq term-default-bg-color (frame-parameter nil 'background-color))
-		  (setq term-default-fg-color (frame-parameter nil 'foreground-color))))))
+       ;; Toogle between line and char mode in term-mode
+       (define-key term-raw-map (kbd "C-'") 'term-line-mode)
+       (define-key term-mode-map (kbd "C-'") 'term-char-mode)
+
+       ;; Use configured color theme
+       (setq term-default-bg-color (frame-parameter nil 'background-color))
+       (setq term-default-fg-color (frame-parameter nil 'foreground-color)))
+
+     (add-hook 'term-mode-hook 'my-setup-term-mode)))
