f78b29bcdc1eeba09a9ef9b453ea1989ac0fae0f
Svend Sorensen <svend@ciffer.net>
2012-05-07 15:11:29 -0700

    Simplify

--- init.d/00notmuch.el
+++ init.d/00notmuch.el
@@ -1,26 +1,21 @@
 (eval-after-load 'notmuch
-  '(progn
-     ;; notmuch-always-prompt-for-sender requires ido-mode
-     ;; Add (ido-mode t) to emacs configuration
-     (setq notmuch-always-prompt-for-sender t)
+   '(progn
+      ;; notmuch-always-prompt-for-sender requires ido-mode
+      ;; Add (ido-mode t) to emacs configuration
+      (setq notmuch-always-prompt-for-sender t)
 
-     ;; Use Bcc instead of Fcc
-     (setq notmuch-fcc-dirs nil)
+      ;; Use Bcc instead of Fcc
+      (setq notmuch-fcc-dirs nil)
 
-     (setq notmuch-saved-searches '(("Personal Inbox (unread)" . "tag:inbox and not tag:uw and not tag:lists and tag:unread")
-				    ("Personal Inbox" . "tag:inbox and not tag:uw and not tag:lists")
-				    ("UW Inbox (unread)" . "tag:inbox and tag:uw and not tag:lists and not from:root and not tag:nagios and tag:unread")
-				    ("UW Inbox" . "tag:inbox and tag:uw and not tag:lists and not from:root and not tag:nagios")
-				    ("CIRG Lists" . "tag:inbox and tag:lists::cirg-adm or tag:lists::cirg-core and tag:inbox")
-				    ("CIRG Nagios" . "tag:inbox and tag:uw and tag:nagios and not tag:lists")
-				    ("Notmuch Inbox" . "tag:inbox and tag:lists::notmuch")
-				    ("Wallop Inbox" . "tag:inbox and tag:lists::wallop_2012")
-				    ("Debian Security Inbox" . "tag:inbox and tag:lists::debian-security-announce")
-				    ("waiting" . "tag:waiting")))
+      (setq notmuch-saved-searches '(("Personal Inbox" . "tag:inbox and not tag:uw and (not tag:lists or (tag:lists::wallop_2012))")
+				     ("UW Inbox" . "tag:inbox and tag:uw and (not tag:lists or (tag:lists::cirg-adm or tag:lists::cirg-core)) and not from:root and not tag:nagios")
+				     ("CIRG Nagios" . "tag:inbox and tag:uw and tag:nagios and not tag:lists")
+				     ("Notmuch Inbox" . "tag:inbox and tag:lists::notmuch")
+				     ("Debian Security Inbox" . "tag:inbox and tag:lists::debian-security-announce")))
 
-     ;; Show newest mail first
-     (setq notmuch-search-oldest-first nil)
+      ;; Show newest mail first
+      (setq notmuch-search-oldest-first nil)
 
-     ;; Notmuch remote setup (on all hosts except garnet)
-     (when (not (string= system-name "garnet.ciffer.net"))
-       (setq notmuch-command "notmuch-remote"))))
+      ;; Notmuch remote setup (on all hosts except garnet)
+      (when (not (string= system-name "garnet.ciffer.net"))
+	(setq notmuch-command "notmuch-remote"))))
