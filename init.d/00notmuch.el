e35c5ce66fe3207720fb320d9f807afc4b2ee6f7
Svend Sorensen <svend@ciffer.net>
2012-05-15 10:48:19 -0700

    Add action and waiting saved searches

--- init.d/00notmuch.el
+++ init.d/00notmuch.el
@@ -9,6 +9,8 @@
 
       (setq notmuch-saved-searches '(("Personal Inbox" . "tag:inbox and not tag:uw and (not tag:lists or (tag:lists::wallop_2012))")
 				     ("UW Inbox" . "tag:inbox and tag:uw and (not tag:lists or (tag:lists::cirg-adm or tag:lists::cirg-core)) and not from:root and not tag:nagios")
+				     ("Action" . "tag:action")
+				     ("Wating" . "tag:waiting")
 				     ("CIRG Nagios" . "tag:inbox and tag:uw and tag:nagios and not tag:lists")
 				     ("Notmuch Inbox" . "tag:inbox and tag:lists::notmuch")
 				     ("Debian Security Inbox" . "tag:inbox and tag:lists::debian-security-announce")))
