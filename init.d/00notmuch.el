aba95a331482b8a7d40b269146d616e141bd8e8a
Svend Sorensen <svend@ciffer.net>
2012-05-15 13:55:59 -0700

    Fix spelling

--- init.d/00notmuch.el
+++ init.d/00notmuch.el
@@ -10,7 +10,7 @@
       (setq notmuch-saved-searches '(("Personal Inbox" . "tag:inbox and not tag:uw and (not tag:lists or (tag:lists::wallop_2012 or tag:lists::cheastyboys))")
 				     ("UW Inbox" . "tag:inbox and tag:uw and (not tag:lists or (tag:lists::cirg-adm or tag:lists::cirg-core)) and not from:root and not tag:nagios")
 				     ("Action" . "tag:action")
-				     ("Wating" . "tag:waiting")
+				     ("Waiting" . "tag:waiting")
 				     ("CIRG Nagios" . "tag:inbox and tag:uw and tag:nagios and not tag:lists")
 				     ("Notmuch Inbox" . "tag:inbox and tag:lists::notmuch")
 				     ("Debian Security Inbox" . "tag:inbox and tag:lists::debian-security-announce")))
