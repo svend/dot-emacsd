bea552a3a56b0d0065f2ee297e7ec6eedee26e67
Svend Sorensen <svend@ciffer.net>
2012-05-30 14:31:19 -0700

    Undo puppet-mode rebinding of RET key

--- init.d/00puppet-mode.el
+++ init.d/00puppet-mode.el
@@ -2,6 +2,10 @@
   '(progn
      (add-hook 'puppet-mode-hook 'flymake-puppet-load)
 
+     ; puppet-mode rebinds RET to newline-and-indent. Remove this
+     ; keybinding.
+     (define-key puppet-mode-map "\C-m" nil)
+
      (defun my-setup-puppet-mode ()
        "My preferences for puppet mode"
        (interactive)
