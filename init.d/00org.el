b74414080a8d8137360c868797a1ad47966c9eaf
Svend Sorensen <svend@ciffer.net>
2012-08-27 11:42:53 -0700

    Enable flyspell for org-mode

--- init.d/00org.el
+++ init.d/00org.el
@@ -2,4 +2,5 @@
   '(progn
      (require 'org-notmuch)
      (setq org-startup-indented t)
-     (add-hook 'org-mode-hook 'auto-fill-mode)))
+     (add-hook 'org-mode-hook 'auto-fill-mode)
+     (add-hook 'org-mode-hook 'turn-on-flyspell 'append)))
