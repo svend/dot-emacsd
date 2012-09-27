2c4a7ab82b00c0f464848895974ebe8a8ec27793
Svend Sorensen <svend@ciffer.net>
2012-09-27 09:44:22 -0700

    Remove emacs 23 color-theme configuration

--- init.d/00color-theme.el
+++
@@ -1,6 +0,0 @@
-(eval-after-load 'color-theme
-  '(progn
-     ;; Initialize color theme after it loads
-     (color-theme-initialize)))
-
-(color-theme-initialize)
--- init.d/50color-theme-solarized.el
+++
@@ -1,3 +0,0 @@
-(autoload 'color-theme-solarized "color-theme-solarized" nil t)
-(autoload 'color-theme-solarized-dark "color-theme-solarized" nil t)
-(autoload 'color-theme-solarized-light "color-theme-solarized" nil t)
