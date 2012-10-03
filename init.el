4c009ba4a125375751c8908bbbe41952f3f23101
Svend Sorensen <svend@ciffer.net>
2012-10-02 20:25:48 -0700

    My emacs configuration requires emacs 24

--- init.el
+++ init.el
@@ -1,9 +1,10 @@
 ;; Emacs settings
 
-(defvar user-emacs-init-path
-  "~/.emacs.d/init.d"
-  "The full path to user init directory")
+(when (>= emacs-major-version 24)
+  (defvar user-emacs-init-path
+    "~/.emacs.d/init.d"
+    "The full path to user init directory")
 
-;; Load all n*.el files under user init directory
-(dolist (file (directory-files user-emacs-init-path t "^[0-9]+.*\\.el$"))
-  (load file))
+  ;; Load all n*.el files under user init directory
+  (dolist (file (directory-files user-emacs-init-path t "^[0-9]+.*\\.el$"))
+    (load file)))
