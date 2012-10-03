e93303f8a0b664c06c99475521e59bf1ee2ac757
Svend Sorensen <svend@ciffer.net>
2012-10-02 20:48:29 -0700

    Move ido save file to ~/.cache

--- init.d/00ido.el
+++ init.d/00ido.el
@@ -1,5 +1,6 @@
 (eval-after-load 'ido
   '(progn
+     (setq ido-save-directory-list-file "~/.cache/emacs/ido.last")
      (setq ido-enable-flex-matching t)
      (setq ido-everywhere t)
      (setq ido-use-virtual-buffers t)))
