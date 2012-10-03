e3b23deafa9a91ce772158248672c4ac4f35bb89
Svend Sorensen <svend@ciffer.net>
2012-10-02 20:37:50 -0700

    Move gnus config to init.d

--- init.d/00gnus.el
+++ init.d/00gnus.el
@@ -1,3 +1,28 @@
 (eval-after-load 'gnus
   '(progn
-     (setq gnus-init-file "~/.emacs.d/gnus")))
+     ;; Gmane
+     (setq gnus-select-method
+	   '(nntp "news.gmane.org"))
+
+     ;; Debugging
+     ;; (setq imap-log t)
+
+     ;; Other accounts, including IMAP
+     (setq gnus-secondary-select-methods
+	   '((nnimap "uw"
+		     (nnimap-address "svends.deskmail.washington.edu")
+		     (nnimap-server-port 993)
+		     (nnimap-stream ssl)
+		     ;; This doesn't work
+		     ;; (nnimap-authenticator gssapi)
+		     (nnimap-authenticator login)
+		     ;; TODO: Playing it safe. Is this required?
+		     (nnimap-expunge-on-close 'never)
+		     (nnimap-list-pattern ("INBOX" "mail/*")))))
+
+     (setq gnus-posting-styles
+	   '((".*"
+	      (address "svend@ciffer.net"))
+	     ("uw"
+	      (address "svends@uw.edu")
+	      ("Bcc" "svends@uw.edu"))))))
