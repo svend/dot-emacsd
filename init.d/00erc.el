d4c34155e85633d1b681285adf1d5f5e4e9611cd
Svend Sorensen <svend@ciffer.net>
2012-10-04 12:29:35 -0700

    Ignore IRC noise

--- init.d/00erc.el
+++ init.d/00erc.el
@@ -1,5 +1,9 @@
 (eval-after-load 'erc
   '(progn
+     ;; Do not notify for join, part, or quit
+     (add-to-list 'erc-track-exclude-types "JOIN")
+     (add-to-list 'erc-track-exclude-types "PART")
+     (add-to-list 'erc-track-exclude-types "QUIT")
 
      ;; Disable erc-track C-c keybindings, which conflict with
      ;; ansi-term
