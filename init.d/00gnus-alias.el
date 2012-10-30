c2abf5347ecdb84bc96b42b9e4410a83d48f3590
Svend Sorensen <svend@ciffer.net>
2012-10-30 11:27:53 -0700

    Use uw identity for email from a UW person

--- init.d/00gnus-alias.el
+++ init.d/00gnus-alias.el
@@ -5,4 +5,5 @@
 	     ("uw" nil "Svend Sorensen <svends@uw.edu>" nil (("Bcc" . "svends@uw.edu")) nil nil)))
      (setq gnus-alias-default-identity "default")
      (setq gnus-alias-identity-rules
-	   '(("uw" ("any" "svends@\\(uw.edu\\|u\\.washington.edu\\|washington\\.edu\\)" both) "uw")))))
+	   '(("to-uw" ("any" "svends@\\(uw.edu\\|u\\.washington.edu\\|washington\\.edu\\)" both) "uw")
+	     ("from-uw" ("from" "@\\(uw.edu\\|u\\.washington.edu\\|washington\\.edu\\)" both) "uw"))))
