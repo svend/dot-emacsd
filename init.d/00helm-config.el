65907f698865327bb714a420e5a02f23c857fb29
Svend Sorensen <svend@ciffer.net>
2013-01-11 15:17:30 -0800

    Update

--- init.d/00helm-config.el
+++ init.d/00helm-config.el
@@ -1,5 +1,6 @@
 (eval-after-load 'helm-files
   '(progn
      (require 'helm-ls-git)
-     (add-to-list 'helm-for-files-preferred-list 'helm-c-source-ls-git t)))
+     (add-to-list 'helm-for-files-preferred-list 'helm-c-source-ls-git t)
+     (delete 'helm-c-source-locate helm-for-files-preferred-list)))
 
