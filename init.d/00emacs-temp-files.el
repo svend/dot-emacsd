d17cbcf115710a31cdcb9e1ca0cca6af0279a45a
Svend Sorensen <svend@ciffer.net>
2013-02-18 15:43:24 -0800

    Comments

--- init.d/00emacs-temp-files.el
+++ init.d/00emacs-temp-files.el
@@ -1,19 +1,22 @@
 ;; Move temporary files to ~/.cache/emacs
 
-;; Put all backups and auto-saves under ~/.cache
+;; Backup and auto-saves
 (setq backup-directory-alist
       `((".*" . ,"~/.cache/emacs/backup/")))
 (setq auto-save-file-name-transforms
       `((".*" ,"~/.cache/emacs/backup/" t)))
 
-;; Put auto-save list file under ~/.cache
+;; Auto-save list files
 (setq auto-save-list-file-prefix "~/.cache/emacs/auto-save-list/.saves-")
 
-;; TODO: Lock files, which can be confused for auto-save files are
-;; created in the same directory as the file being edited. I don't
-;; know of any way to disable this feature.
+;; Lock files
+;;
+;; http://www.gnu.org/software/emacs/manual/html_node/elisp/File-Locks.html
+;;
+;; TODO: Lock are created in the same directory as the file being
+;; edited. I don't know of any way to disable this feature.
 ;;
 ;; http://lists.gnu.org/archive/html/emacs-devel/2011-07/msg01020.html
 
-;; Put url cache file under ~/.cache
+;; URL cache
 (setq url-cache-directory "~/.cache/emacs/url")
