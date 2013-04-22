df4babd48411760533677c7e1f087feeecd25e5e
Svend Sorensen <svend@ciffer.net>
2013-04-21 20:19:21 -0700

    Remove whitespace

--- init.d/00helm-config.el
+++ init.d/00helm-config.el
@@ -3,4 +3,3 @@
      (require 'helm-ls-git)
      (add-to-list 'helm-for-files-preferred-list 'helm-c-source-ls-git t)
      (delete 'helm-c-source-locate helm-for-files-preferred-list)))
-
--- init.d/50puppet-mode.el
+++ init.d/50puppet-mode.el
@@ -1,6 +1,2 @@
 (autoload 'puppet-mode "puppet-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
-
-
-
-
