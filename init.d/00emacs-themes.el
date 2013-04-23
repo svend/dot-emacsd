12844f4b276d84307a01938190d36c567d995ac3
Svend Sorensen <svend@ciffer.net>
2013-04-22 19:56:37 -0700

    Remove user theme loading

--- init.d/00emacs-themes.el
+++ init.d/00emacs-themes.el
@@ -1,6 +1,1 @@
-;; Add all vendor themes to load-path
-(dolist (file (directory-files "~/.emacs.d/themes" t "\\w+"))
-  (when (file-directory-p file)
-    (add-to-list 'custom-theme-load-path file)))
-
 (load-theme 'solarized-light t)
