d672fe1707ab5716602d119a077b1571b2fbc07b
Svend Sorensen <svend@ciffer.net>
2013-04-24 16:37:41 -0700

    Move global modes to after init hook

--- init.d/90global-modes.el
+++ init.d/90global-modes.el
@@ -1,5 +1,3 @@
-(helm-mode 1)
-(projectile-global-mode)
-(add-hook 'after-init-hook #'global-flycheck-mode)
-(require 'helm-config)
-(add-hook 'after-init-hook #'global-flycheck-mode)
+(add-hook 'after-init-hook 'global-flycheck-mode)
+(add-hook 'after-init-hook 'helm-mode)
+(add-hook 'after-init-hook 'projectile-global-mode)
