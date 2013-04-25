75155f69fda618ee5e6e0f3b5236aa44674e9433
Svend Sorensen <svend@ciffer.net>
2013-04-24 21:29:03 -0700

    Load all files under init.d, not just files staring with a number

--- init.el
+++ init.el
@@ -5,6 +5,6 @@
     "~/.emacs.d/init.d"
     "The full path to user init directory")
 
-  ;; Load all n*.el files under user init directory
-  (dolist (file (directory-files user-emacs-init-path t "^[0-9]+.*\\.el$"))
+  ;; Load all *.el files under user init directory
+  (dolist (file (directory-files user-emacs-init-path t "\\.el$"))
     (load file)))
