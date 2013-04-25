a11e3990d0389c8bfdf1fbbd320942d8a8d45b25
Svend Sorensen <svend@ciffer.net>
2013-04-24 21:25:07 -0700

    Consolidate auto-mode list

--- init.d/50mr.el
+++
@@ -1,2 +0,0 @@
-;; Open mr config files in conf-mode
-(add-to-list 'auto-mode-alist '(".mrconfig" . conf-mode))
--- init.d/auto-mode.el
+++ init.d/auto-mode.el
@@ -1,2 +1,4 @@
+(add-to-list 'auto-mode-alist '(".mrconfig" . conf-mode))
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.mdwn$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
