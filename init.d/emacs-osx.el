722fc3da1d340bf5e92043b81ad010e5052978c5
Svend Sorensen <svend@ciffer.net>
2013-05-01 15:44:43 -0700

    Copy GPG_AGENT_INFO env on OS X

--- init.d/emacs-osx.el
+++ init.d/emacs-osx.el
@@ -4,5 +4,6 @@
 
   ;; fix the PATH variable
   (when (memq window-system '(mac ns))
-    (exec-path-from-shell-initialize))
+    (exec-path-from-shell-initialize)
+    (exec-path-from-shell-copy-env "GPG_AGENT_INFO"))
   )
