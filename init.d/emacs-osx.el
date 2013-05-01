858484b4ccf6145caa3ca77302d61b9548e13712
Svend Sorensen <svend@ciffer.net>
2013-05-01 10:03:37 -0700

    Ignore stderr when getting PATH

--- init.d/emacs-osx.el
+++ init.d/emacs-osx.el
@@ -4,7 +4,7 @@
 
   ;; fix the PATH variable
   (defun set-exec-path-from-shell-PATH ()
-    (let ((path-from-shell (shell-command-to-string "$SHELL -l -i -c 'echo $PATH'")))
+    (let ((path-from-shell (shell-command-to-string "$SHELL -l -i -c 'echo $PATH' 2>/dev/null")))
       (setenv "PATH" path-from-shell)
       (setq exec-path (split-string path-from-shell path-separator))))
 
