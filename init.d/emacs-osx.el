d77724859d520abf2c0b7791875b9f51057c2061
Svend Sorensen <svend@ciffer.net>
2013-05-01 10:34:11 -0700

    Use exec-path-from-shell package

--- init.d/emacs-osx.el
+++ init.d/emacs-osx.el
@@ -3,10 +3,6 @@
   (setq mac-command-modifier 'control)
 
   ;; fix the PATH variable
-  (defun set-exec-path-from-shell-PATH ()
-    (let ((path-from-shell (shell-command-to-string "$SHELL -l -i -c 'echo $PATH' 2>/dev/null")))
-      (setenv "PATH" path-from-shell)
-      (setq exec-path (split-string path-from-shell path-separator))))
-
-  (if window-system (set-exec-path-from-shell-PATH))
+  (when (memq window-system '(mac ns))
+    (exec-path-from-shell-initialize))
   )
