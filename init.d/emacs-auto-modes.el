ba88c39f69334babe9309fbe73a9f51bb24143a1
Svend Sorensen <svend@ciffer.net>
2013-05-01 16:01:04 -0700

    More ruby autoloads

--- init.d/emacs-auto-modes.el
+++ init.d/emacs-auto-modes.el
@@ -1,5 +1,15 @@
-(add-to-list 'auto-mode-alist '(".mrconfig" . conf-mode))
+(add-to-list 'auto-mode-alist '(".mrconfig$" . conf-mode))
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.mdwn$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
-(add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
+
+;; From https://github.com/bbatsov/emacs-dev-kit/blob/master/ruby-config.el
+;; Rake files are ruby, too, as are gemspecs, rackup files, and gemfiles.
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
+;; Vagrantfile is ruby
+(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
