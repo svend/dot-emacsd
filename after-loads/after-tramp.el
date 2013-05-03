18055e016134fa318b70f740000c8a0edb12993e
Svend Sorensen <svend@ciffer.net>
2013-05-03 10:16:47 -0700

    scpc method is deprecated

--- after-loads/after-tramp.el
+++ after-loads/after-tramp.el
@@ -1,28 +1,6 @@
 ;; Put tramp history file under ~/.cache
 (setq tramp-persistency-file-name "~/.cache/emacs/tramp")
 
-;; Set default connection method to scpc (scp with ControlMaster)
-;;
-;; TODO: What is the best method (scpc, scp, ssh)?
-;;
-;; "External transfer methods are normally preferable to inline
-;; transfer methods, giving better performance." (ssh is inline,
-;; scp and scpc is external.)
-;;
-;; http://www.xemacs.org/Documentation/packages/html/tramp_4.html#SEC10
-;;
-;; "My suggestion is to use an inline method. For large files,
-;; out-of-band methods might be more efficient, but I guess that
-;; most people will want to edit mostly small files.
-;;
-;; I guess that these days, most people can access a remote
-;; machine by using ssh. So I suggest that you use the `ssh'
-;; method."
-;;
-;; http://www.xemacs.org/Documentation/packages/html/tramp_4.html#SEC11
-;;
-(setq tramp-default-method "scpc")
-
 ;; Edit remote files via sudo
 ;;
 ;; Enable editing of remote files as root using sudo using the
