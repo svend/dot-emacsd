c1890cb4e69ec3cb788217c7a927595e6b8275b3
Svend Sorensen <svend@ciffer.net>
2013-05-02 22:09:56 -0700

    Disable Emacs lock files (yay!)

--- init.d/emacs-temp-files.el
+++ init.d/emacs-temp-files.el
@@ -14,9 +14,10 @@
 ;; http://www.gnu.org/software/emacs/manual/html_node/elisp/File-Locks.html
 ;;
 ;; Locks are created in the same directory as the file being
-;; edited. There is no way to disable this.
+;; edited. They can be disabled as of 24.3.
 ;;
 ;; http://lists.gnu.org/archive/html/emacs-devel/2011-07/msg01020.html
+(setq create-lockfiles nil)
 
 ;; URL cache
 (setq url-cache-directory "~/.cache/emacs/url")
