ef4a7cf89d17ea2834f0d2e40844881c3f296898
Svend Sorensen <svend@ciffer.net>
2013-05-19 18:47:42 -0700

    Use default completion

--- after-loads/after-projectile.el
+++ after-loads/after-projectile.el
@@ -1,4 +1,4 @@
 (setq projectile-known-projects-file "~/.cache/emacs/projectile-bookmarks.eld")
 (projectile-load-known-projects)
 
-(setq projectile-completion-system 'helm)
+(setq projectile-completion-system 'default)
