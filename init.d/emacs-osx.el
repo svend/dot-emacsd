ceaba2fe78a5b53cbd5a822890c749c55cfdf7b3
Svend Sorensen <svend@ciffer.net>
2013-05-21 12:48:05 -0700

    Update

--- init.d/emacs-osx.el
+++ init.d/emacs-osx.el
@@ -1,7 +1,8 @@
+;; Set Mac keyboard
 (when (eq system-type 'darwin)
   (setq mac-option-modifier 'meta)
-  (setq mac-command-modifier 'control)
+  (setq mac-command-modifier 'control))
 
-  ;; fix the PATH variable
-  (when (memq window-system '(mac ns))
-    (exec-path-from-shell-initialize)))
+;; fix the PATH variable
+(when (eq window-system 'ns)
+  (exec-path-from-shell-initialize))
