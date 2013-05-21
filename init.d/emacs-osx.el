6b259a802dabe800772a6303ecf8cc573823e240
Svend Sorensen <svend@ciffer.net>
2013-05-21 12:40:28 -0700

    Update

--- init.d/emacs-osx.el
+++ init.d/emacs-osx.el
@@ -4,9 +4,4 @@
 
   ;; fix the PATH variable
   (when (memq window-system '(mac ns))
-    (exec-path-from-shell-initialize)
-    (exec-path-from-shell-copy-env "ALTERNATE_EDITOR")
-    (exec-path-from-shell-copy-env "EDITOR")
-    (exec-path-from-shell-copy-env "GPG_AGENT_INFO")
-    (exec-path-from-shell-copy-env "SSH_AUTH_SOCK"))
-  )
+    (exec-path-from-shell-initialize)))
