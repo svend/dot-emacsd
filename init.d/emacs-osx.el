f579709dc204460491c27accaaf01ef62cb22b5a
Svend Sorensen <svend@ciffer.net>
2013-05-23 09:18:22 -0700

    Fix syntax

--- init.d/emacs-osx.el
+++ init.d/emacs-osx.el
@@ -2,7 +2,8 @@
 (when (eq system-type 'darwin)
   (setq ns-command-modifier 'control)
   ;; This is the default
-  ;; (setq ns-alternate-modifier 'meta))
+  ;; (setq ns-alternate-modifier 'meta)
+  )
 
 ;; fix the PATH variable
 (when (eq window-system 'ns)
