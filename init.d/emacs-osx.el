a46ea7a8dbb447fdd5b0b93f57cc490ea3e424d5
Svend Sorensen <svend@ciffer.net>
2013-05-23 08:33:39 -0700

    Comment out default

--- init.d/emacs-osx.el
+++ init.d/emacs-osx.el
@@ -1,7 +1,8 @@
 ;; Set Mac keyboard
 (when (eq system-type 'darwin)
-  (setq mac-command-modifier 'control)
-  (setq mac-option-modifier 'meta))
+  (setq ns-command-modifier 'control)
+  ;; This is the default
+  ;; (setq ns-alternate-modifier 'meta))
 
 ;; fix the PATH variable
 (when (eq window-system 'ns)
