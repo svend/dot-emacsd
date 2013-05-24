97f28ee8857e19cd2aa90a9972f30d12c8b04b15
Svend Sorensen <svend@ciffer.net>
2013-05-24 07:07:32 -0700

    Remove mac key modifications
    Trying caps/enter as control keys.

--- init.d/emacs-osx.el
+++ init.d/emacs-osx.el
@@ -1,10 +1,3 @@
-;; Set Mac keyboard
-(when (eq system-type 'darwin)
-  (setq ns-command-modifier 'control)
-  ;; This is the default
-  ;; (setq ns-alternate-modifier 'meta)
-  )
-
 ;; fix the PATH variable
 (when (eq window-system 'ns)
   (exec-path-from-shell-initialize))
