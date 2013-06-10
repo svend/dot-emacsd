9229f5736040066fc81a0694c6dc6768f291b73c
Svend Sorensen <svend@ciffer.net>
2013-06-10 10:49:15 -0700

    Remove duplicate

--- after-loads/after-exec-path-from-shell.el
+++ after-loads/after-exec-path-from-shell.el
@@ -1,5 +1,4 @@
 (add-to-list 'exec-path-from-shell-variables "ALTERNATE_EDITOR")
 (add-to-list 'exec-path-from-shell-variables "EDITOR")
-(add-to-list 'exec-path-from-shell-variables "ALTERNATE_EDITOR")
 (add-to-list 'exec-path-from-shell-variables "GPG_AGENT_INFO")
 (add-to-list 'exec-path-from-shell-variables "SSH_AUTH_SOCK")
