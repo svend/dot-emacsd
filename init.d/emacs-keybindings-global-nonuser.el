63684228f53ad8a71adb0360c38fbb18b223cfa8
Svend Sorensen <svend@ciffer.net>
2013-07-03 09:23:01 -0700

    Add buffer-move keybindings

--- init.d/emacs-keybindings-global-nonuser.el
+++ init.d/emacs-keybindings-global-nonuser.el
@@ -6,9 +6,14 @@
 ;;
 ;; http://www.gnu.org/software/emacs/manual/html_node/elisp/Key-Binding-Conventions.html
 
-(global-set-key (kbd "<C-mouse-4>") 'global-text-scale-increase)
-(global-set-key (kbd "<C-mouse-5>") 'global-text-scale-decrease)
-(global-set-key (kbd "C--") 'er/contract-region)
-(global-set-key (kbd "C-=") 'er/expand-region)
-(global-set-key (kbd "C-x 7") 'my-rotate-windows)
-(global-set-key (kbd "C-`") 'helm-for-files)
+(global-set-key (kbd "<C-mouse-4>")	'global-text-scale-increase)
+(global-set-key (kbd "<C-mouse-5>")	'global-text-scale-decrease)
+; expand-region
+(global-set-key (kbd "C--")		'er/contract-region)
+(global-set-key (kbd "C-=")		'er/expand-region)
+(global-set-key (kbd "C-`")		'helm-for-files)
+; buffer-move
+(global-set-key (kbd "<C-S-up>")	'buf-move-up)
+(global-set-key (kbd "<C-S-down>")	'buf-move-down)
+(global-set-key (kbd "<C-S-left>")	'buf-move-left)
+(global-set-key (kbd "<C-S-right>")	'buf-move-right)
