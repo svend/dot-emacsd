bd4d4356af8de16f83078377974c3ba5ba260afe
Svend Sorensen <svend@ciffer.net>
2013-07-12 14:14:58 -0700

    org-mode config update

--- after-loads/after-org.el
+++ after-loads/after-org.el
@@ -1,3 +1,10 @@
 (require 'org-notmuch)
-(setq org-startup-indented t)
 (add-hook 'org-mode-hook 'auto-fill-mode)
+(setq org-src-fontify-natively t)
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((perl . t)
+   (ruby . t)
+   (sh . t)
+   (python . t)
+   (emacs-lisp . t)))
