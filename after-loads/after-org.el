3bc8a6ff4e16ed2c7e93c5b58e8bd368619dccd7
Svend Sorensen <svend@ciffer.net>
2013-07-19 08:18:45 -0700

    Don't indent src blocks

--- after-loads/after-org.el
+++ after-loads/after-org.el
@@ -1,7 +1,8 @@
 (require 'org-notmuch)
 (add-hook 'org-mode-hook 'auto-fill-mode)
-(setq org-src-fontify-natively t)
 (setq org-replace-disputed-keys t)
+(setq org-src-fontify-natively t)
+(setq org-src-preserve-indentation t)
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((perl . t)
