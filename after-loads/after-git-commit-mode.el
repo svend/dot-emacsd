0ad5646532cc9f2af09e26f87a5aeb0b0e2949f5
Svend Sorensen <svend@ciffer.net>
2013-07-19 15:06:33 -0700

    Update

--- after-loads/after-git-commit-mode.el
+++ after-loads/after-git-commit-mode.el
@@ -1,2 +1,3 @@
 ;; Spell check git commit messages
-(add-hook 'git-commit-mode-hook 'flyspell-mode)
+(add-hook 'git-commit-mode-hook 'turn-on-flyspell)
+(add-hook 'git-commit-mode-hook '(lambda () (message "hello")))
