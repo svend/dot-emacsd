3929dcf3be7fd69fcfe2163f99f7c4f674e81b8a
Svend Sorensen <svend@ciffer.net>
2013-09-06 09:40:28 -0700

    Add INFOPATH to env

--- after-loads/after-exec-path-from-shell.el
+++ after-loads/after-exec-path-from-shell.el
@@ -1,4 +1,5 @@
 (add-to-list 'exec-path-from-shell-variables "ALTERNATE_EDITOR")
 (add-to-list 'exec-path-from-shell-variables "EDITOR")
 (add-to-list 'exec-path-from-shell-variables "GPG_AGENT_INFO")
+(add-to-list 'exec-path-from-shell-variables "INFOPATH")
 (add-to-list 'exec-path-from-shell-variables "SSH_AUTH_SOCK")
