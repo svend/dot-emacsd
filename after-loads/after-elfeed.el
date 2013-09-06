a4965c2d2045c5a2f28650d06894d5c9b421258a
Svend Sorensen <svend@ciffer.net>
2013-09-06 10:15:14 -0700

    Add elfeed feeds

---
+++ after-loads/after-elfeed.el
@@ -0,0 +1,6 @@
+(setq elfeed-feeds
+      '("http://git-annex.branchable.com/devblog/index.atom"
+	"http://planet.emacsen.org/atom.xml"
+	"http://www.reddit.com/r/emacs/.rss"
+	"http://www.reddit.com/r/ruby/.rss"
+	"http://planet.debian.org/atom.xml"))
