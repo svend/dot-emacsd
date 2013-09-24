ec57a4dc7c7fac890c4e1b123cab4e4ce7ff3d45
Svend Sorensen <svend@ciffer.net>
2013-09-24 08:13:12 -0700

    Enable helm-adaptive-mode

--- after-loads/after-helm.el
+++ after-loads/after-helm.el
@@ -2,3 +2,6 @@
 (add-to-list 'helm-for-files-preferred-list 'helm-source-ls-git t)
 (delete 'helm-source-locate helm-for-files-preferred-list)
 (helm-attrset 'follow 1 helm-source-buffers-list)
+
+(setq helm-adaptive-history-file "~/.cache/emacs/helm-adaptive-history")
+(helm-adaptative-mode 1)
