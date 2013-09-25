d3d8f35f60b2f214af69fc1b428bd720316a1dd7
Svend Sorensen <svend@ciffer.net>
2013-09-25 15:32:07 -0700

    Configure ERC passwords

--- after-loads/after-erc.el
+++ after-loads/after-erc.el
@@ -1,2 +1,6 @@
-;; Flyspell mode
+(erc-services-mode 1)
 (erc-spelling-mode 1)
+
+(setq erc-prompt-for-nickserv-password nil)
+(setq erc-nickserv-passwords
+      `((BitlBee ((,bitlbee-username . ,bitlbee-password)))))
