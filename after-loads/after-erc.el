f16f06d8678f2979e648edebbe01276a0a10cb25
Svend Sorensen <svend@ciffer.net>
2013-09-26 15:30:51 -0700

    More ERC changes

--- after-loads/after-erc.el
+++ after-loads/after-erc.el
@@ -1,6 +1,10 @@
 (erc-services-mode 1)
+(erc-scrolltobottom-mode 1)
 (erc-spelling-mode 1)
 
+(setq erc-hide-list '("JOIN" "MODE" "PART" "QUIT"))
+
+;; Nickserv configuration
 (setq erc-prompt-for-nickserv-password nil)
 (setq erc-nickserv-passwords
       `((BitlBee ((,bitlbee-username . ,bitlbee-password)))
