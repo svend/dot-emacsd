4b20f0e638d2298c76964ad5b85d98e273b9ad4e
Svend Sorensen <svend@ciffer.net>
2013-10-07 18:40:20 -0700

    Update

--- after-loads/after-projectile.el
+++ after-loads/after-projectile.el
@@ -1,4 +1,7 @@
 (setq projectile-known-projects-file "~/.cache/emacs/projectile-bookmarks.eld")
 (projectile-load-known-projects)
 
+;; Use my default (helm) instead of projectile default (ido)
 (setq projectile-completion-system 'default)
+;; Use helm instead of projectile-find-file
+;;(setq projectile-switch-project-action 'helm-projectile)
