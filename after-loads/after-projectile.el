6af2efb54b1594e77d4c56bba77142999b83f658
Svend Sorensen <svend@ciffer.net>
2013-12-20 09:49:47 -0800

    Projectile updates

--- after-loads/after-projectile.el
+++ after-loads/after-projectile.el
@@ -1,7 +1,7 @@
 (setq projectile-known-projects-file (expand-file-name "projectile-bookmarks.eld" emacs-cache-dir))
 (projectile-load-known-projects)
 
-;; Use my default (helm) instead of projectile default (ido)
-(setq projectile-completion-system 'default)
-;; Use helm instead of projectile-find-file
-;;(setq projectile-switch-project-action 'helm-projectile)
+;; ;; Use my default (helm) instead of projectile default (ido)
+;; (setq projectile-completion-system 'default)
+(setq projectile-switch-project-action 'projectile-vc-dir)
+(setq projectile-use-git-grep t)
