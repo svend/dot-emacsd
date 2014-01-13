fba3349b8f5e21ea8f9641afa69410ae509015fa
Svend Sorensen <svend@ciffer.net>
2014-01-13 12:38:34 -0800

    Use source code pro font

--- init.d/emacs-osx.el
+++ init.d/emacs-osx.el
@@ -1,4 +1,4 @@
 (when (eq window-system 'ns)
   ;; fix the PATH variable
   (exec-path-from-shell-initialize)
-  (set-face-attribute 'default nil :font "Menlo-14"))
+  (set-face-attribute 'default nil :font "Source Code Pro" :height 140))
