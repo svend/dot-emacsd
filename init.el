db90368866c8c2b1f0a8bb98c52086f4be0bc160
Svend Sorensen <svend@ciffer.net>
2014-01-14 16:48:18 -0800

    Try org-mode emacs init file

--- init.el
+++ init.el
@@ -1,10 +1,5 @@
-;; Emacs settings
+;; Load org-mode init file
 
 (when (>= emacs-major-version 24)
-  (defvar user-emacs-init-path
-    "~/.emacs.d/init.d"
-    "The full path to user init directory")
-
-  ;; Load all *.el files under user init directory
-  (dolist (file (directory-files user-emacs-init-path t "\\.el\\(\\.gpg\\)?$"))
-    (load file)))
+  (org-babel-load-file (expand-file-name "emacs-init.org"
+					 user-emacs-directory)))
