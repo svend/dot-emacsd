f060e30f5bdb6fac23d61ee1c97655929e864dec
Svend Sorensen <svend@ciffer.net>
2014-01-15 17:03:09 -0800

    Move to org

--- init.d/sh.el
+++
@@ -1,9 +0,0 @@
-(defun my-setup-sh-mode ()
-  "My preferences for sh-mode"
-  (interactive)
-  (setq sh-basic-offset 8)
-  (setq sh-indentation 8)
-  (setq sh-indent-for-case-alt '+)
-  (setq sh-indent-for-case-label 0))
-
-(add-hook 'sh-mode-hook 'my-setup-sh-mode)
