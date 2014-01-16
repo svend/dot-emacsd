91c96a48fca6dc90de69a401f794e87e737e064c
Svend Sorensen <svend@ciffer.net>
2014-01-15 17:31:25 -0800

    Move to org

--- init.d/emacs-paths.el
+++
@@ -1,7 +0,0 @@
-;; Add all vendor subdirectories to load-path
-(dolist (file (directory-files "~/.emacs.d/vendor" t "\\w+"))
-  (when (file-directory-p file)
-    (add-to-list 'load-path file)))
-
-(defvar emacs-cache-dir "~/.cache/emacs"
-  "Path to store temporary emacs files.")
