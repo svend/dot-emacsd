c6169e84f1bbaa5e375ee55d94bb831e875efc2b
Svend Sorensen <svend@ciffer.net>
2014-01-23 10:00:20 -0800

    Solarized updates

--- after-loads/after-solarized.el
+++
@@ -1,6 +0,0 @@
-(setq solarized-use-variable-pitch nil)
-(setq solarized-height-minus-1 1.0)
-(setq solarized-height-plus-1 1.0)
-(setq solarized-height-plus-2 1.0)
-(setq solarized-height-plus-3 1.0)
-(setq solarized-height-plus-4 1.0)
