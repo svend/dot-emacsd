d60fe30f4b841e1b6b4b4548d43388a824654773
Svend Sorensen <svend@ciffer.net>
2014-02-03 23:37:48 -0800

    Use password-store.el

--- after-loads/after-erc.el
+++ after-loads/after-erc.el
@@ -3,6 +3,11 @@
 
 (setq erc-hide-list '("JOIN" "MODE" "PART" "QUIT"))
 
+(setq bitlbee-username (password-store-copy "bitlbee-username"))
+(setq bitlbee-password (password-store-copy "bitlbee-password"))
+(setq freenode-username (password-store-copy "freenode/username"))
+(setq freenode-password (password-store-copy "freenode/password"))
+
 ;; Nickserv configuration
 (setq erc-prompt-for-nickserv-password nil)
 (setq erc-nickserv-passwords
