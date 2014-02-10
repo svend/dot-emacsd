18fc1c4e4d222602fa7da083a3df3e89a373aebc
Svend Sorensen <svend@ciffer.net>
2014-02-09 23:25:27 -0800

    Formatting

--- after-loads/after-erc.el
+++ after-loads/after-erc.el
@@ -3,10 +3,10 @@
 
 (setq erc-hide-list '("JOIN" "MODE" "PART" "QUIT"))
 
-(setq bitlbee-username (password-store-get "bitlbee-username"))
-(setq bitlbee-password (password-store-get "bitlbee-password"))
-(setq freenode-username (password-store-get "freenode/username"))
-(setq freenode-password (password-store-get "freenode/password"))
+(setq bitlbee-username (password-store-get "bitlbee-username")
+      bitlbee-password (password-store-get "bitlbee-password")
+      freenode-username (password-store-get "freenode/username")
+      freenode-password (password-store-get "freenode/password"))
 
 ;; Nickserv configuration
 (setq erc-prompt-for-nickserv-password nil)
