5938a9f34db0817b59d653d1055beb1f82b3f473
Svend Sorensen <svend@ciffer.net>
2014-03-04 12:14:42 -0800

    Update

--- after-loads/after-helm.el
+++
@@ -1,7 +0,0 @@
-(require 'helm-ls-git)
-(add-to-list 'helm-for-files-preferred-list 'helm-source-ls-git t)
-(delete 'helm-source-locate helm-for-files-preferred-list)
-(helm-attrset 'follow 1 helm-source-buffers-list)
-
-(setq helm-adaptive-history-file (expand-file-name "helm-adaptive-history" emacs-cache-dir))
-(helm-adaptative-mode 1)
