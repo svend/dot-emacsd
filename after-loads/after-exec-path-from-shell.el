b1aa6b72766346decf5798da3634ffc5dc4211e1
Svend Sorensen <svend@ciffer.net>
2014-03-03 17:15:13 -0800

    Move exec-path-from-shell to main config file

--- after-loads/after-exec-path-from-shell.el
+++
@@ -1,7 +0,0 @@
-(add-to-list 'exec-path-from-shell-variables "ALTERNATE_EDITOR")
-(add-to-list 'exec-path-from-shell-variables "EDITOR")
-(add-to-list 'exec-path-from-shell-variables "GPG_AGENT_INFO")
-(add-to-list 'exec-path-from-shell-variables "INFOPATH")
-(add-to-list 'exec-path-from-shell-variables "LANG")
-(add-to-list 'exec-path-from-shell-variables "LC_ALL")
-(add-to-list 'exec-path-from-shell-variables "SSH_AUTH_SOCK")
