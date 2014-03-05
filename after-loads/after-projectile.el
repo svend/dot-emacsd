9af764af8ae93aee5fd9c673884aedda173d81ba
Svend Sorensen <svend@ciffer.net>
2014-03-05 13:20:11 -0800

    Move projectile config

--- after-loads/after-projectile.el
+++
@@ -1,7 +0,0 @@
-(setq projectile-known-projects-file (expand-file-name "projectile-bookmarks.eld" emacs-cache-dir))
-(projectile-load-known-projects)
-
-;; ;; Use my default (helm) instead of projectile default (ido)
-;; (setq projectile-completion-system 'default)
-(setq projectile-switch-project-action 'projectile-vc)
-(setq projectile-use-git-grep t)
