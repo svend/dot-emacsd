53afa688351f6e6e342af9791578f6dc26b12a9e
Svend Sorensen <svend@ciffer.net>
2014-04-14 16:27:33 -0700

    Update

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ediff-window-setup-function (quote ediff-setup-windows-plain)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
