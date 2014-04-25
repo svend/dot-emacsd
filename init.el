fefb3072b41818a1ea973eb3f1799e768d7c89d4
Svend Sorensen <svend@ciffer.net>
2014-04-25 15:16:09 -0700

    Merged upstream

--- init.el
+++ init.el
@@ -15,5 +15,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(linum ((t (:inherit (shadow default) :weight light))))
- '(vertical-border ((t (:foreground "#babdb6")))))
+ '(linum ((t (:inherit (shadow default) :weight light)))))
