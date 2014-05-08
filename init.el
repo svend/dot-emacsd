b08f175a1c86f5945a0e5b6a8c4222c3660e14ca
Svend Sorensen <svend@ciffer.net>
2014-05-08 12:29:41 -0700

    Horizontal ediff

--- init.el
+++ init.el
@@ -9,6 +9,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ediff-split-window-function (quote split-window-horizontally))
  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
  '(jabber-alert-presence-hooks nil)
  '(jabber-auto-reconnect t)
