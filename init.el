18f065e98bcba5d9c7825d95467817581533b342
Svend Sorensen <svend@ciffer.net>
2014-05-25 03:16:36 -0700

    Set fixed with font to "Source Code Pro"

--- init.el
+++ init.el
@@ -25,6 +25,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(fixed-pitch ((t (:family "Source Code Pro"))))
  '(linum ((t (:inherit (shadow default) :weight light))))
  '(org-table ((t (:inherit fixed-pitch))))
  '(variable-pitch ((t (:family "Verdana")))))
