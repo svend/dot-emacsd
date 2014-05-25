1e84119d8a4453013b967b6963c1874b11749463
Svend Sorensen <svend@ciffer.net>
2014-05-25 03:07:18 -0700

    Move to customize

--- init.el
+++ init.el
@@ -25,4 +25,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(linum ((t (:inherit (shadow default) :weight light)))))
+ '(linum ((t (:inherit (shadow default) :weight light))))
+ '(org-table ((t (:inherit fixed-pitch))))
+ '(variable-pitch ((t (:family "Verdana")))))
