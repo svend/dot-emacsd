a7ed9a7154468df46a694a93af1e685b178a67f5
Svend Sorensen <svend@ciffer.net>
2014-05-25 04:09:31 -0700

    Fonts

--- init.el
+++ init.el
@@ -25,7 +25,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:height 130 :family "Verdana"))))
  '(fixed-pitch ((t (:family "Source Code Pro"))))
  '(linum ((t (:inherit (shadow default) :weight light))))
+ '(org-checkbox ((t (:inherit (bold fixed-pitch)))))
  '(org-table ((t (:inherit fixed-pitch))))
  '(variable-pitch ((t (:family "Verdana")))))
