fb6c065f7ea26c1b7fdaca83c7998e107b0944d5
Svend Sorensen <svend@ciffer.net>
2014-05-25 23:13:49 -0700

    Update

--- init.el
+++ init.el
@@ -25,10 +25,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 130 :family "Verdana"))))
- '(fixed-pitch ((t (:family "Source Code Pro"))))
+ '(default ((t (:inherit nil :stipple nil :background "#ffffff" :foreground "#000000" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "nil" :family "DejaVu Sans"))))
+ '(defaul ((t (:family "DejaVu Sans"))))
+ '(fixed-pitch ((t (:family "DejaVu Sans Mono"))))
  '(linum ((t (:inherit (shadow default) :weight light))))
  '(org-checkbox ((t (:inherit (bold fixed-pitch)))))
  '(org-document-title ((t (:height 1.2))))
  '(org-table ((t (:inherit fixed-pitch))))
- '(variable-pitch ((t (:family "Verdana")))))
+ '(variable-pitch ((t (:family "DejaVu Sans")))))
