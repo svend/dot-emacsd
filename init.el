fc2163fa4142ee2b8084262b773093183a1a941a
Svend Sorensen <svend@ciffer.net>
2014-06-10 17:42:35 -0700

    Set default font size to 130

--- init.el
+++ init.el
@@ -36,7 +36,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans"))))
+ '(default ((t (:height 130 :family "DejaVu Sans"))))
  '(fixed-pitch ((t (:family "DejaVu Sans Mono"))))
  '(jabber-activity-face ((t (:foreground "#edd400" :weight bold))))
  '(jabber-activity-personal-face ((t (:foreground "#8ae234" :weight bold))))
