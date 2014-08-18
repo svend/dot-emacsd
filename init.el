da1c3384b82e2b0ba3ef4344eddf14932dc383fc
Svend Sorensen <svend@ciffer.net>
2014-08-18 15:57:35 -0700

    Remove nonworking customization

--- init.el
+++ init.el
@@ -9,7 +9,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(alert-default-style (quote notifier))
  '(comint-input-ring-size 10000)
  '(custom-enabled-themes (quote (tango-plus)))
  '(ediff-split-window-function (quote split-window-horizontally))
