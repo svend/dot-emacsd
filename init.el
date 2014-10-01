ca1e9a4fcb7b78231db8747fcca12d3d7828a569
Svend Sorensen <svend@ciffer.net>
2014-10-01 09:30:42 -0700

    Move fonts to org file

--- init.el
+++ init.el
@@ -32,8 +32,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 113 :family "DejaVu Sans"))))
- '(fixed-pitch ((t (:family "DejaVu Sans Mono"))))
  '(jabber-activity-face ((t (:foreground "#edd400" :weight bold))))
  '(jabber-activity-personal-face ((t (:foreground "#8ae234" :weight bold))))
  '(jabber-chat-prompt-foreign ((t (:foreground "#204a87" :weight bold))))
