cd2183594654d465f7e172516419d6eb532059a0
Svend Sorensen <svend@ciffer.net>
2014-11-07 23:04:29 -0800

    Fixed pitch for diffs

--- init.el
+++ init.el
@@ -32,6 +32,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(diff-changed ((t (:inherit fixed-pitch))))
+ '(diff-context ((t (:inherit fixed-pitch))))
  '(font-lock-doc-face ((t (:inherit (font-lock-string-face variable-pitch)))))
  '(jabber-activity-face ((t (:foreground "#edd400" :weight bold))))
  '(jabber-activity-personal-face ((t (:foreground "#8ae234" :weight bold))))
