bb1cb610648e1b3ab4ff5d38ff9829c893fa7f1b
Svend Sorensen <svend@ciffer.net>
2015-03-10 11:28:20 -0700

    Add simple theme

---
+++ simple-theme.el
@@ -0,0 +1,9 @@
+(deftheme simple
+  "Created 2015-03-10.")
+
+(custom-theme-set-faces
+ 'simple
+ `(fringe ((t (:background ,(face-background 'default) :foreground ,(face-foreground 'shadow)))))
+ `(linum ((t (:background ,(face-background 'default) :foreground ,(face-foreground 'shadow) :weight light)))))
+
+(provide-theme 'simple)
