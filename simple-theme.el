664251a87d9b53c60554636abc236c0bed001b2d
Svend Sorensen <svend@ciffer.net>
2015-03-10 14:53:00 -0700

    Flycheck face overrides

--- simple-theme.el
+++ simple-theme.el
@@ -3,6 +3,9 @@
 
 (custom-theme-set-faces
  'simple
+ `(flycheck-fringe-error ((t (:background ,(face-background 'error) :foreground ,(face-foreground 'error)))))
+ `(flycheck-fringe-warning ((t (:background ,(face-background 'warning) :foreground ,(face-foreground 'warning)))))
+ `(cursor ((t (:background ,(face-foreground 'default)))))
  `(fringe ((t (:background ,(face-background 'default) :foreground ,(face-foreground 'shadow)))))
  `(linum ((t (:background ,(face-background 'default) :foreground ,(face-foreground 'shadow) :weight light :family "Sans Serif")))))
 
