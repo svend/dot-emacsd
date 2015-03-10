0db6d27a4d585c664fca4639a846f187ec52cc14
Svend Sorensen <svend@ciffer.net>
2015-03-10 16:09:29 -0700

    Update

--- simple-theme.el
+++ simple-theme.el
@@ -5,6 +5,7 @@
  'simple
  `(flycheck-fringe-error ((t (:background ,(face-background 'error) :foreground ,(face-foreground 'error)))))
  `(flycheck-fringe-warning ((t (:background ,(face-background 'warning) :foreground ,(face-foreground 'warning)))))
+ `(flycheck-fringe-info ((t (:background ,(face-background 'success) :foreground ,(face-foreground 'success)))))
  `(cursor ((t (:background ,(face-foreground 'default)))))
  `(fringe ((t (:background ,(face-background 'default) :foreground ,(face-foreground 'shadow)))))
  `(linum ((t (:background ,(face-background 'default) :foreground ,(face-foreground 'shadow) :weight light :family "Sans Serif")))))
