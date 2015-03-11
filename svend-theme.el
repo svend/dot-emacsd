cd9bb47ac1c1088112b677960ccbe1420fb310f6
Svend Sorensen <svend@ciffer.net>
2015-03-10 21:31:03 -0700

    svend-theme

---
+++ svend-theme.el
@@ -0,0 +1,46 @@
+(deftheme svend
+  "Created 2015-03-10.")
+
+(custom-theme-set-variables
+ 'svend
+ '(comint-input-ignoredups t)
+ '(comint-input-ring-size 10000)
+ '(cursor-type (quote bar))
+ '(dired-dwim-target t)
+ '(enable-local-variables :safe)
+ '(history-length 10000)
+ '(jabber-alert-presence-hooks nil)
+ '(jabber-auto-reconnect t)
+ '(jabber-history-enabled t)
+ '(org-default-notes-file "~/doc/org/notes.org")
+ '(org-directory "~/doc/org")
+ '(org-reverse-note-order t)
+ '(org-src-window-setup (quote other-window))
+ '(safe-local-variable-values (quote ((encoding . utf-8))))
+ '(electric-pair-mode t))
+
+(custom-theme-set-faces
+ 'svend
+ '(font-lock-doc-face ((t (:inherit (font-lock-string-face variable-pitch)))))
+ '(jabber-activity-face ((t (:foreground "#edd400" :weight bold))))
+ '(jabber-activity-personal-face ((t (:foreground "#8ae234" :weight bold))))
+ '(jabber-chat-prompt-foreign ((t (:foreground "#204a87" :weight bold))))
+ '(jabber-chat-prompt-local ((t (:foreground "#75507b" :weight bold))))
+ '(jabber-chat-prompt-system ((t (:foreground "#4e9a06" :weight bold))))
+ '(jabber-title-large ((t (:inherit variable-pitch :weight bold :height 2.0 :width ultra-expanded))))
+ '(jabber-title-medium ((t (:inherit variable-pitch :weight bold :height 1.6 :width expanded))))
+ '(linum ((t (:inherit (shadow default) :weight light))))
+ '(org-block ((t (:inherit fixed-pitch))))
+ '(org-block-background ((t (:inherit fixed-pitch))))
+ '(org-checkbox ((t (:inherit (bold fixed-pitch)))))
+ '(org-code ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-title ((t (:weight bold :height 1.4))))
+ '(org-level-1 ((t (:height 1.3))))
+ '(org-level-2 ((t (:height 1.2))))
+ '(org-level-3 ((t (:height 1.1))))
+ '(org-table ((t (:inherit fixed-pitch))))
+ '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
+ '(sh-heredoc ((t (:foreground "#8f5902"))))
+ '(show-paren-match ((t (:background "#fce94f" :foreground "#000000")))))
+
+(provide-theme 'svend)
