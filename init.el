50d858eef35de2f6299753589cfb1aaefdcd7f6d
Svend Sorensen <svend@ciffer.net>
2015-06-26 12:20:17 -0700

    Update

--- init.el
+++ init.el
@@ -1,5 +1,11 @@
 ;; Load org-mode init file
 
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;; (package-initialize)
+
 (when (>= emacs-major-version 24)
   (require 'org)
   (org-babel-load-file
