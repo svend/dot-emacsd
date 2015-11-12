293be15e7bb90e16667ea5396aac6ee6e8200bc0
Svend Sorensen <svend@ciffer.net>
2015-11-12 11:22:00 -0800

    More font themes

---
+++ svend-font-fira-theme.el
@@ -0,0 +1,10 @@
+(deftheme svend-font-fira
+  "Created 2015-03-11.")
+
+(custom-theme-set-faces
+ 'svend-font-fira
+ '(default ((t (:family "Fira"))))
+ '(fixed-pitch ((t (:family "Fira Mono"))))
+ '(variable-pitch ((t (:family "Fira")))))
+
+(provide-theme 'svend-font-fira)
