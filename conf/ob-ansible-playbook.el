9c09f99a23b3fa7bed8ab3f8b8146d288e04d8e6
Svend Sorensen <svend@ciffer.net>
2016-05-31 16:16:13 -0700

    Support inventory

--- conf/ob-ansible-playbook.el
+++ conf/ob-ansible-playbook.el
@@ -1,10 +1,13 @@
 (require 'ob)
 
 (defun org-babel-execute:ansible-playbook (body params)
-  (let ((in-file (org-babel-temp-file "ansible-playbook-")))
+  (let ((inventory (cdr (assoc :inventory params)))
+        (in-file (org-babel-temp-file "ansible-playbook-")))
     (with-temp-file in-file (insert body))
     (shell-command-to-string
-     (format "ansible-playbook %s" in-file))))
+     (concat "ansible-playbook"
+             (when inventory (format " --inventory %s" inventory))
+             (format " %s" in-file)))))
 
 (defun org-babel-prep-session:ansible-playbook (_session _params)
   (error "Ansible does not support sessions"))
