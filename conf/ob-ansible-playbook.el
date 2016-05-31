f8832a56d505afb4fce833bc71e8f53b5d902a42
Svend Sorensen <svend@ciffer.net>
2016-05-31 15:57:43 -0700

    Update

--- conf/ob-ansible-playbook.el
+++ conf/ob-ansible-playbook.el
@@ -4,7 +4,10 @@
   (let ((in-file (org-babel-temp-file "ansible-playbook-")))
     (with-temp-file in-file (insert body))
     (shell-command-to-string
-     (format "ansible-playbook --inventory , %s" in-file))))
+     (format "ansible-playbook %s" in-file))))
+
+(defun org-babel-prep-session:ansible-playbook (_session _params)
+  (error "Ansible does not support sessions"))
 
 (provide 'ob-ansible-playbook)
 ;;; ob-ansible-playbook.el ends here
