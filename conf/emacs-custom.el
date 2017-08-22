74c9428726936d154712772e440dee643fe4a77d
Svend Sorensen <svend@ciffer.net>
2017-08-22 10:35:13 -0700

    Do not prompt to kill processes

--- conf/emacs-custom.el
+++ conf/emacs-custom.el
@@ -5,6 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(comint-input-ignoredups t)
  '(comint-input-ring-size 10000)
+ '(confirm-kill-processes nil)
  '(cursor-type (quote bar))
  '(custom-enabled-themes (quote (svend-font-dejavu svend tango-plus)))
  '(dired-dwim-target t)
@@ -16,6 +17,8 @@
  '(jabber-auto-reconnect t)
  '(jabber-history-enabled t)
  '(jabber-keepalive-interval 30)
+ '(js-indent-level 2)
+ '(json-reformat:indent-width 2)
  '(ns-pop-up-frames nil)
  '(org-default-notes-file "~/doc/org/notes.org")
  '(org-directory "~/doc/org")
