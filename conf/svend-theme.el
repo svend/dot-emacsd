c9c717504dff860176ca881a7d1f653f25d628d4
Svend Sorensen <ssorensen@whitepages.com>
2019-03-21 16:43:39 -0700

    Revert to fixed pitch

--- conf/svend-theme.el
+++ conf/svend-theme.el
@@ -12,7 +12,7 @@
  `(flyspell-incorrect ((t (:background ,(face-background 'default)))))
  '(font-lock-doc-face ((t (:inherit variable-pitch))))
  `(fringe ((t (:background ,(face-background 'default) :foreground ,(face-foreground 'shadow)))))
- `(line-number ((t (:background ,(face-background 'default) :foreground ,(face-foreground 'shadow) :weight light :inherit (variable-pitch shadow default)))))
+ `(line-number ((t (:background ,(face-background 'default) :foreground ,(face-foreground 'shadow) :weight light :inherit (fixed-pitch shadow default)))))
  '(markdown-pre-face ((t (:inherit fixed-pitch))))
  '(nobreak-space ((t (:underline nil))))
  '(org-block ((t (:inherit fixed-pitch))))
