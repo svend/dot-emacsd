9a29c6c8982b610acfaecb28fddd6ab254fc026c
Svend Sorensen <svend@svends.net>
2020-02-25 12:35:20 -0800

    Move to init file

--- conf/emacs-custom.el
+++ conf/emacs-custom.el
@@ -3,11 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(confirm-kill-processes nil)
  '(ediff-split-window-function (quote split-window-horizontally))
  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
  '(ffap-machine-p-known (quote reject))
- '(history-length 10000)
  '(ns-pop-up-frames nil)
  '(reb-re-syntax (quote string)))
 (custom-set-faces
