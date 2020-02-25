0df21dca54463f4116ac21309cac4692416bd2bd
Svend Sorensen <svend@svends.net>
2020-02-25 08:25:52 -0800

    Move to init file

--- conf/emacs-custom.el
+++ conf/emacs-custom.el
@@ -3,24 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(comint-input-ignoredups t)
- '(comint-input-ring-size 10000)
  '(confirm-kill-processes nil)
- '(dired-dwim-target t)
  '(ediff-split-window-function (quote split-window-horizontally))
  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
  '(ffap-machine-p-known (quote reject))
  '(history-length 10000)
- '(jabber-alert-presence-hooks nil)
- '(jabber-auto-reconnect t)
- '(jabber-history-enabled t)
- '(jabber-keepalive-interval 30)
  '(ns-pop-up-frames nil)
- '(org-default-notes-file "~/doc/org/notes.org")
- '(org-directory "~/doc/org")
- '(org-reverse-note-order t)
- '(reb-re-syntax (quote string))
- '(safe-local-variable-values (quote ((encoding . utf-8)))))
+ '(reb-re-syntax (quote string)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
