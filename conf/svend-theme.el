33887d69910c7cc979febbe95b5e50a1c8205c54
Svend Sorensen <svend@svends.net>
2020-07-21 10:07:57 -0700

    Tweaks to theme

--- conf/svend-theme.el
+++ conf/svend-theme.el
@@ -14,23 +14,19 @@
  '(font-lock-doc-face ((t (:inherit variable-pitch))))
  `(fringe ((t (:background ,(face-background 'default) :foreground ,(face-foreground 'shadow)))))
  `(line-number ((t (:background ,(face-background 'default) :foreground ,(face-foreground 'shadow) :weight light :inherit (fixed-pitch shadow default)))))
- ;; lsp-ui-sideline-code-action foreground is yellow, which is difficult to see
- `(lsp-ui-sideline-code-action ((t (:foreground ,(face-foreground 'shadow) :underline t))))
- '(lsp-ui-sideline-current-symbol ((t (:inherit lsp-ui-sideline-symbol :weight ultra-bold))))
  '(markdown-pre-face ((t (:inherit fixed-pitch))))
  '(nobreak-space ((t (:underline nil))))
- '(org-block ((t (:inherit fixed-pitch))))
- '(org-block-background ((t (:inherit fixed-pitch))))
+ ;; See Emacs 27 NEWS: ** New face attribute ':extend' to control face extension at EOL.
+ '(org-block ((t (:inherit fixed-pitch :extend t))))
+ '(org-block-begin-line ((t (:extend t))) )
+ '(org-block-end-line ((t (:extend t))) )
  '(org-checkbox ((t (:inherit fixed-pitch))))
  '(org-code ((t (:inherit fixed-pitch))))
  '(org-date ((t (:inherit fixed-pitch))))
- '(org-document-title ((t (:weight bold :height 1.4))))
  '(org-ellipsis ((t (:underline nil))))
- '(org-level-1 ((t (:height 1.3))))
- '(org-level-2 ((t (:height 1.2))))
- '(org-level-3 ((t (:height 1.1))))
  '(org-table ((t (:inherit fixed-pitch))))
  '(org-verbatim ((t (:inherit fixed-pitch))))
+
  '(sh-heredoc ((t (:inherit font-lock-string-face)))))
 
 (provide-theme 'svend)
